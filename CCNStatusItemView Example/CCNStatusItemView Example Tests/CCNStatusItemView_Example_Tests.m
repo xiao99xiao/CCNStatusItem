84f03e01256f7f9d0a84171f03f8254299c0d6ee
Frank Gregor <phranck@cocoanaut.com>
2014-12-31 01:23:08 +0100

    Added: Test Target

---
+++ CCNStatusItemView Example/CCNStatusItemView Example Tests/CCNStatusItemView_Example_Tests.m
@@ -0,0 +1,39 @@
+//
+//  CCNStatusItemView_Example_Tests.m
+//  CCNStatusItemView Example Tests
+//
+//  Created by Frank Gregor on 31.12.14.
+//  Copyright (c) 2014 cocoa:naut. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface CCNStatusItemView_Example_Tests : XCTestCase
+@end
+
+@implementation CCNStatusItemView_Example_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+//    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+}
+
+@end
