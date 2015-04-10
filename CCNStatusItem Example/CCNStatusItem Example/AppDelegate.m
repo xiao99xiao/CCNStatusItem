832fbf4a4a77b239aff78d49308763875107241f
Frank Gregor <phranck@cocoanaut.com>
2015-04-10 16:37:12 +0200

    Changed: updated example due to CCNStatusItem changes

--- CCNStatusItem Example/CCNStatusItem Example/AppDelegate.m
+++ CCNStatusItem Example/CCNStatusItem Example/AppDelegate.m
@@ -21,7 +21,6 @@
     CCNStatusItemWindowAppearance *appearance = [CCNStatusItemWindowAppearance defaultAppearance];
 //    appearance.cornerRadius = 85.0;
     appearance.presentationTransition = CCNPresentationTransitionSlideAndFade;
-    appearance.backgroundColor = [NSColor colorWithCalibratedRed:0.196 green:0.200 blue:0.204 alpha:0.950];
     [CCNStatusItem setWindowAppearance:appearance];
 
     [CCNStatusItem presentStatusItemWithImage:[NSImage imageNamed:@"statusbar-icon"]
@@ -29,7 +28,7 @@
 }
 
 - (IBAction)enableDisableCheckboxAction:(id)sender {
-    [CCNStatusItem sharedInstance].appearsDisabled = (self.enableDisableCheckbox.state == NSOnState);
+    [CCNStatusItem sharedInstance].statusItem.button.appearsDisabled = (self.enableDisableCheckbox.state == NSOnState);
 }
 
 @end
