48ade22046c3c3314956f9038d318eed1064934c
Frank Gregor <phranck@cocoanaut.com>
2015-08-18 22:33:54 +0200

    Added: Example for custom view as statusItem

--- CCNStatusItem Example/CCNStatusItem Example/AppDelegate.m
+++ CCNStatusItem Example/CCNStatusItem Example/AppDelegate.m
@@ -22,6 +22,8 @@
 
 @property (assign) BOOL proximityDetectionEnabled;
 @property (readwrite, assign) NSInteger proximitySliderValue;
+
+@property NSView *customItemView;
 @end
 
 @implementation AppDelegate
@@ -31,9 +33,13 @@
     CCNStatusItem *sharedItem = [CCNStatusItem sharedInstance];
     sharedItem.windowConfiguration.presentationTransition = CCNPresentationTransitionSlideAndFade;
     sharedItem.proximityDragDetectionHandler = [self proximityDragDetectionHandler];
-    [sharedItem presentStatusItemWithImage:[NSImage imageNamed:@"statusbar-icon"]
-                     contentViewController:[ContentViewController viewController]
-                               dropHandler:nil];
+
+    
+    // Uncomment this to get a status item with an image.
+    [self presentStatusItemWithImage];
+    
+    // Uncomment this to get a status item with a custom view.
+//    [self presentStatusItemWithCustomView];
     
     
     // restore GUI elements
@@ -42,6 +48,65 @@
     self.appearsDisabledCheckbox.state = (sharedItem.appearsDisabled ? NSOnState : NSOffState);
     self.disableCheckbox.state = (sharedItem.enabled ? NSOffState : NSOnState);
     [self.presentationTransitionRadios selectCellAtRow:(NSInteger)sharedItem.windowConfiguration.presentationTransition column:0];
+}
+
+- (void)presentStatusItemWithImage {
+    CCNStatusItem *sharedItem = [CCNStatusItem sharedInstance];
+    [sharedItem presentStatusItemWithImage:[NSImage imageNamed:@"statusbar-icon"]
+                     contentViewController:[ContentViewController viewController]
+                               dropHandler:nil];
+}
+
+- (void)presentStatusItemWithCustomView {
+    NSImageView *imageView = [[NSImageView alloc] init];
+    imageView.image = [NSImage imageNamed:@"statusbar-icon"];
+    imageView.translatesAutoresizingMaskIntoConstraints = NO;
+    NSTextField *textField = [[NSTextField alloc] init];
+    
+    NSMutableParagraphStyle *paragraphStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+    paragraphStyle.alignment = NSCenterTextAlignment;
+    paragraphStyle.minimumLineHeight = 19;
+    
+    NSDictionary *attributes = @{ NSFontAttributeName: [NSFont boldSystemFontOfSize:14.0], NSParagraphStyleAttributeName: paragraphStyle };
+    textField.attributedStringValue = [[NSAttributedString alloc] initWithString:@"CCNStatusItem" attributes:attributes];
+    textField.translatesAutoresizingMaskIntoConstraints = NO;
+    textField.backgroundColor = [NSColor clearColor];
+    textField.bordered = NO;
+    textField.editable = NO;
+    textField.selectable = NO;
+    
+    // FIXME: I have absolutely no idea why the heck the width of that calculated rect is shorter than the given attributed string!
+    NSRect textFieldRect = [textField.attributedStringValue boundingRectWithSize:NSMakeSize(CGFLOAT_MAX, [NSStatusBar systemStatusBar].thickness)
+                                                                         options:NSStringDrawingUsesLineFragmentOrigin];
+    
+    CGFloat imageWidth = imageView.image.size.width;
+    CGFloat textFieldWidth = textFieldRect.size.width;
+    CGFloat systemStatusBarHeight = [NSStatusBar systemStatusBar].thickness;
+    NSInteger padding = 2;
+    CGFloat customItemViewWidth = padding + imageWidth + ceilf(NSWidth(textFieldRect)) + padding;
+    
+    
+    self.customItemView = [[NSView alloc] initWithFrame:NSMakeRect(0, 0, customItemViewWidth, systemStatusBarHeight)];
+    [self.customItemView addSubview:imageView];
+    [self.customItemView addSubview:textField];
+    
+    NSDictionary *views = @{
+        @"imageView": imageView,
+        @"textField": textField,
+    };
+    NSDictionary *metrics = @{
+        @"imageWidth": @(imageWidth),
+        @"systemStatusBarHeight": @(systemStatusBarHeight),
+        @"textFieldWidth": @(textFieldWidth),
+        @"padding": @(padding),
+    };
+    [self.customItemView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(padding)-[imageView(imageWidth)][textField(textFieldWidth)]-(padding)-|" options:0 metrics:metrics views:views]];
+    [self.customItemView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[imageView(systemStatusBarHeight)]|" options:0 metrics:metrics views:views]];
+    [self.customItemView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[textField(systemStatusBarHeight)]|" options:0 metrics:metrics views:views]];
+    
+    CCNStatusItem *sharedItem = [CCNStatusItem sharedInstance];
+    [sharedItem presentStatusItemWithView:self.customItemView
+                    contentViewController:[ContentViewController viewController]];
 }
 
 - (CCNStatusItemProximityDragDetectionHandler)proximityDragDetectionHandler {
